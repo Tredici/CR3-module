5881cc79919f5c2dbce994dfc15dc975f5273f2b
Tredici <marcolucio1999@gmail.com>
2021-03-01 00:50:25 +0100

    Implemented init and exit functions.
    I've implemented init_cr3_module and exit_cr3_module.

--- cr3-mod.c
+++ cr3-mod.c
@@ -1,7 +1,27 @@
-
+/* da includere obbligatoriamente nel codice di un modulo */
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h> /* module_init */
 
 
 /* licenza */
 MODULE_LICENSE("GPL");
 
+/* entry point - __init specifica che non serve più dopo l'avvio */
+static int __init init_cr3_module(void)
+{
+    printk("Starting CR3 module...");
 
+    return 0;
+}
+
+/* exit point - __exit specifica che serve solo per la rimozione */
+static void __exit exit_cr3_module(void)
+{
+    printk("Terminating CR3 module...");
+}
+
+/* per specificare le funzioni da chiamare quando si carica
+ * e si rimuove il modulo */
+module_init(init_cr3_module); /* all'avvio va chiamata questa funzione */
+module_exit(exit_cr3_module); /* viene chiamato all'uscita */
